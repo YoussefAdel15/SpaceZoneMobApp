7e7603454680f54af1ecd1ed570ec4bc895dbd64
Youssef Adel <67727073+YoussefAdel15@users.noreply.github.com>
2023-06-11 17:10:28 +0300

    fix secure input

--- components/AppTextInput.js
+++ components/AppTextInput.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TextInput, StyleSheet } from 'react-native';
-import { COLORS } from '../constants/colors';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import * as Font from 'expo-font';
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import { COLORS } from "../constants/colors";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import * as Font from "expo-font";
 
 const Input = ({
   label,
@@ -13,14 +13,14 @@ const Input = ({
   onFocus = () => {},
   ...props
 }) => {
-  const [hidePassword, setHidePassword] = useState(true); // Use a single state variable for password and confirmation
+  const [hidePassword, setHidePassword] = useState(false); // Use a single state variable for password and confirmation
   const [isFocused, setIsFocused] = useState(false);
   const [fontLoaded, setFontLoaded] = useState(false);
 
   useEffect(() => {
     const loadFonts = async () => {
       await Font.loadAsync({
-        'Sora-SemiBold': require('../assets/fonts/Sora-SemiBold.ttf'),
+        "Sora-SemiBold": require("../assets/fonts/Sora-SemiBold.ttf"),
       });
       setFontLoaded(true);
     };
@@ -39,13 +39,9 @@ const Input = ({
         style={[
           style.inputContainer,
           {
-            borderColor: error
-              ? "red"
-              : isFocused
-                ? "#7978B5"
-                : "#1F41BB",
-            alignItems: 'center',
-            borderWidth: 1
+            borderColor: error ? "red" : isFocused ? "#7978B5" : "#1F41BB",
+            alignItems: "center",
+            borderWidth: 1,
           },
         ]}
       >
@@ -61,26 +57,39 @@ const Input = ({
           }}
           onBlur={() => setIsFocused(false)}
           secureTextEntry={hidePassword}
+          {...((password || passwordConfirmation) && {
+            secureTextEntry: !hidePassword,
+          })}
           style={{ color: "#1C0A00", flex: 1 }}
           {...props}
         />
-        {(password || passwordConfirmation) && ( 
+        {(password || passwordConfirmation) && (
           <Icon
             onPress={() => setHidePassword(!hidePassword)}
-            name={hidePassword ? 'eye-off-outline' : 'eye-outline'}
-            style={{ color: "#7978B5", fontSize: 22, fontFamily: "Sora-SemiBold" }}
+            name={hidePassword ? "eye-off-outline" : "eye-outline"}
+            style={{
+              color: "#7978B5",
+              fontSize: 22,
+              fontFamily: "Sora-SemiBold",
+            }}
           />
         )}
       </View>
       {error && (
-        <Text style={{ marginTop: 7, color: "red", fontSize: 12, fontFamily: "Sora-SemiBold" }}>
+        <Text
+          style={{
+            marginTop: 7,
+            color: "red",
+            fontSize: 12,
+            fontFamily: "Sora-SemiBold",
+          }}
+        >
           {error}
         </Text>
       )}
     </View>
   );
 };
-
 
 const style = StyleSheet.create({
   label: {
@@ -91,11 +100,10 @@ const style = StyleSheet.create({
   inputContainer: {
     height: 55,
     backgroundColor: "#F3F4FB",
-    flexDirection: 'row',
+    flexDirection: "row",
     paddingHorizontal: 15,
     borderWidth: 0.5,
-    borderRadius:15,
-    
+    borderRadius: 15,
   },
 });
 
